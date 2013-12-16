fbccc77d3d787bfec737dd5334b03a76bbc43fb2
RonGokhale <cips173@gmail.com>
2013-12-16 09:16:04 +0400

    1st

---
+++ system/etc/init.insmod.sh
@@ -0,0 +1,7 @@
+#!/system/bin/sh
+
+insmod /system/lib/modules/texfat.ko
+
+insmod /system/lib/modules/moc_platform_mod.ko
+insmod /system/lib/modules/moc_crypto.ko
+insmod /system/lib/modules/dm-crypt.ko
